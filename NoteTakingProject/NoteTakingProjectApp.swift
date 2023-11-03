1884fe9e36f25c5202fd71f17e396748b1c4895c
sumeyraltas <sumeyraltas@gmail.com>
2023-11-03 16:10:27 +0300

    Initial Commit

---
+++ NoteTakingProject/NoteTakingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTakingProjectApp.swift
+//  NoteTakingProject
+//
+//  Created by Sumeyra Altas on 3.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct NoteTakingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
