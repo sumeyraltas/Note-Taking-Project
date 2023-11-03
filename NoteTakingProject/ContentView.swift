0f382c59f1a6c2b0f834d8295f9b1533fe2c4889
sumeyraltas <sumeyraltas@gmail.com>
2023-11-03 16:43:10 +0300

    The outline of the application has been made.

--- NoteTakingProject/ContentView.swift
+++ NoteTakingProject/ContentView.swift
@@ -1,24 +1,99 @@
-//
-//  ContentView.swift
-//  NoteTakingProject
-//
-//  Created by Sumeyra Altas on 3.11.2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var notes: [Note] = []
+    @State private var isAddingNote = false
+    @State private var newNoteTitle = ""
+    @State private var mainTitle = "MY NOTES"
+    @State private var newNoteContent = ""
+    @State private var isDarkMode = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(notes) { note in
+                    NavigationLink(destination: NoteDetail(note: note, isDarkMode: $isDarkMode)) {
+                        Text(note.title)
+                            .foregroundColor(isDarkMode ? .white : .black)
+                    }
+                }
+                .onDelete(perform: deleteNote)
+            }
+          
+            .navigationBarItems(leading:
+                Button(action: {
+                    isDarkMode.toggle()
+                }) {
+                    Image(systemName: isDarkMode ? "sun.max.fill" : "moon.fill")
+                },
+                trailing:
+                Button(action: {
+                    isAddingNote = true
+                }) {
+                    Image(systemName: "plus")
+                }
+            )  .navigationBarTitle(mainTitle)
+            .background(isDarkMode ? Color.black : Color.white)
         }
-        .padding()
+        .sheet(isPresented: $isAddingNote) {
+            NavigationView {
+                Form {
+                    Section(header: Text("New Note")) {
+                        TextField("Title", text: $newNoteTitle)
+                        TextEditor(text: $newNoteContent)
+                    }
+                }
+                .navigationBarTitle("Add Note")
+                .navigationBarItems(leading:
+                    Button("Cancel") {
+                        isAddingNote = false
+                    },
+                    trailing:
+                    Button("Save") {
+                        let newNote = Note(title: newNoteTitle, content: newNoteContent)
+                        notes.append(newNote)
+                        isAddingNote = false
+                        newNoteTitle = ""
+                        newNoteContent = ""
+                    }
+                )
+            }
+        }
+        .environment(\.editMode, .constant(EditMode.active))
+    }
+    
+    func deleteNote(at offsets: IndexSet) {
+        notes.remove(atOffsets: offsets)
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct Note: Identifiable {
+    let id = UUID()
+    var title: String
+    var content: String
+}
+
+struct NoteDetail: View {
+    var note: Note
+    @Binding var isDarkMode: Bool // Add a binding to isDarkMode
+
+    var body: some View {
+        VStack {
+            Text(note.title)
+                .font(.title)
+                .foregroundColor(isDarkMode ? .white : .black)
+            
+            Text(note.content)
+                .padding()
+                .foregroundColor(isDarkMode ? .white : .black)
+            Spacer()
+        }
+        .navigationTitle("Note Detail")
+    }
 }
