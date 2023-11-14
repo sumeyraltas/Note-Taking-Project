45b9054b4b321bd9cf425268fed7d33f829354c0
sumeyraltas <sumeyraltas@gmail.com>
2023-11-14 16:31:58 +0300

    NoteDetail.swift file added.

---
+++ NoteTakingProject/NoteDetail.swift
@@ -0,0 +1,18 @@
+//
+//  NoteDetail.swift
+//  NoteTakingProject
+//
+//  Created by Sumeyra Altas on 14.11.2023.
+//
+
+import SwiftUI
+
+struct NoteDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteDetail()
+}
